dbbb4a176f6459761eaf6ab75a3b531bf126a7ec
Jakob Fischer <jakob@automorph.info>
2013-02-27 14:40:38 +0100

    new file

---
+++ interface_topana.R
@@ -0,0 +1,9 @@
+# author: jakob fischer (jakob@automorph.info)
+# date: 27. February 2013
+# description: 
+# R-tools for handling export of jrnf-networks to 
+# calculate elementary modes with metatool and handle
+# import of results....
+
+
+
