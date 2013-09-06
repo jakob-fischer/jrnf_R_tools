b7ce41d9e5d431b48a6f8c3741457383631e8dbb
Jakob Fischer <jakob@automorph.info>
2013-09-06 13:39:02 +0200

    commented interface_topana.R

--- interface_topana.R
+++ interface_topana.R
@@ -3,23 +3,32 @@
 # description: 
 # R-tools for handling export of jrnf-networks to 
 # calculate elementary modes with metatool and handle
-# import of results....
+# import of results.
 
+
+# Transforms a vector of products or educts id and a vector
+# of asocciated multiplicities to an string that can be read
+# from metatool
 
 wjtm_m_rpart_to_string <- function(co, co_mul, species) {
     bla <- ""
 
-    for(i in 1:length(co)) {
-        for(j in 1:co_mul[i]) {
-            bla <- paste(bla, " ", as.character(species$name[co[i]]), " ", sep="")
+    if(length(co))
+        for(i in 1:length(co)) {
+            for(j in 1:co_mul[i]) {
+                bla <- paste(bla, " ", as.character(species$name[co[i]]), " ", sep="")
 
-            if(i != length(co) || j !=co_mul[i])
-                bla <- paste(bla, " + ", sep="")
-        }  
-    }
+                if(i != length(co) || j !=co_mul[i])
+                    bla <- paste(bla, " + ", sep="")
+            }  
+        }
 
     return(bla)
 }
+
+
+# Transforms a reaction row out of the jrnf reaction data-frame 
+# to a string readable by metatool
 
 wjtm_m_reaction_to_string <- function(reaction, species) {
     bla <- ""
@@ -43,7 +52,7 @@ wjtm_m_reaction_to_string <- function(reaction, species) {
 
 
 
-# TODO
+# Writes a jrnf-network given by network to 
 
 write_jrnf_to_metatool <- function(network, filename, input, output) {
     sp <- network[[1]]
@@ -57,7 +66,7 @@ write_jrnf_to_metatool <- function(network, filename, input, output) {
     writeLines("-ENZIRREV", con=con)
     ac <- ""
 
-    nxt <- nrow(sp) + length(input) + length(output)
+    nxt <- nrow(re) + length(input) + length(output)
     for(i in 1:nxt) 
         ac <- paste(ac, "R", as.character(i), " ", sep="")
 
@@ -72,16 +81,16 @@ write_jrnf_to_metatool <- function(network, filename, input, output) {
     writeLines("", con=con)
 
     writeLines("-CAT", con=con)
-    for(i in 1:nrow(sp)) 
+    for(i in 1:nrow(re))
         writeLines(paste("R", as.character(i), " : ", wjtm_m_reaction_to_string(re[i,], sp), " ", sep=""), con=con)
 
-    nxt <- nrow(sp)+1
+    nxt <- nrow(re)+1
     for(i in input) {
          writeLines(paste("R", as.character(nxt), " : = ", sp$name[i], sep=""), con=con)
          nxt <- nxt+1
     }
 
-    for(i in 1:output) {
+    for(i in output) {
          writeLines(paste("R", as.character(nxt), " :", sp$name[i], "   = ", sep=""), con=con)
          nxt <- nxt+1
     }
@@ -96,8 +105,9 @@ read_elementary_modes <- function(filename) {
     lines <- readLines(con)
     first_em <- grep("ELEMENTARY MODE", lines)+3
 
-    if(is.na(first_em)) {
+    if(length(first_em) && is.na(first_em)) {
         cat("Did not find elementary mode in input file!")
+        close(con)
         return(NA)
     }
 
@@ -107,6 +117,7 @@ read_elementary_modes <- function(filename) {
 
     if(first_size == 0) {
         cat("Elementary modes set found empty!")
+        close(con)
         return(NA)
     }
 
@@ -125,7 +136,7 @@ read_elementary_modes <- function(filename) {
 
 
 
-calculate_elementary_modes <- function(network, in_names, out_names, ext_names) {
+calculate_elementary_modes <- function(network, in_names=c(), out_names=c(), ext_names=c()) {
     in_vector <- logical(length=nrow(network[[1]]))
     out_vector <- logical(length=nrow(network[[1]]))
     
@@ -133,7 +144,7 @@ calculate_elementary_modes <- function(network, in_names, out_names, ext_names) 
         li <- which(i == network[[1]]$name)
 
         if(length(li) > 0)
-            in_vector[li] = TRUE
+            in_vector[li] <- TRUE
         else 
             cat("Error in calculate_elementary_modes: could not find ", i) 
     }
@@ -142,15 +153,40 @@ calculate_elementary_modes <- function(network, in_names, out_names, ext_names) 
         li <- which(i == network[[1]]$name)
 
         if(length(li) > 0)
-            out_vector[li] = TRUE
+            out_vector[li] <- TRUE
         else 
             cat("Error in calculate_elementary_modes: could not find ", i) 
     }
 
+    for(i in ext_names) {
+        li <- which(i == network[[1]]$name)
 
-    write_jrnf_to_metatool(network, "tmp.dat", in_vector, out_vector)
+        if(length(li) > 0) {
+            in_vector[li] <- TRUE
+            out_vector[li] <- TRUE
+        } else 
+            cat("Error in calculate_elementary_modes: could not find ", i) 
+    }
+
+
+    write_jrnf_to_metatool(network, "tmp.dat", which(in_vector), which(out_vector))
 
     system("./metatool/fasttool tmp.dat tmp_out.dat")
 
     em <- read_elementary_modes("tmp_out.dat")
+
+    return(em)
+}
+
+
+
+
+
+print_em <- function(em_matrix, jrnf_network, em_id=1) {
+    j <- as.vector(em_matrix[em_id,])
+    
+    for(i in 1:length(j)) {
+        if(j[i] != 0)
+            cat(j[i], "times: ", wjtm_m_reaction_to_string(jrnf_network[[2]][i,],jrnf_network[[1]]), "\n")
+    }
 }
