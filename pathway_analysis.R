d05dad448ab7a59b62223df99e5ee57091e47b89
Jakob Fischer <jakob@automorph.info>
2014-05-08 20:56:25 +0200

    fixed pa_subpath_decomposition

--- pathway_analysis.R
+++ pathway_analysis.R
@@ -39,13 +39,10 @@ pa_write_em <- function(net, em) {
     x <- (apply(N != 0, 2, sum) == 1)     # remove reactions with just one species involved
     em[x] <- 0    
 
-    # subfunction for printing reaction <i> of network <net>
-    pa_write_rea <- function(i) {  pa_write_rea(net, i, N)  }
-
     # list all (remaining) reactions with coefficients 
     for(i in which(em != 0)) {
         cat(em[i], "X : ")
-        pa_write_rea(i);      
+        pa_write_rea(net, i, N);      
         cat("\n")
     }
 
@@ -82,7 +79,7 @@ vec_gcd <- function(x) {
 # The species that can not be produced and those who can not be consumed are 
 # printed out...
 
-check_reachability <- function(N, path_M) { 
+pa_check_reachability <- function(N, path_M) { 
     x <- N %*% t(path_M)
 
     cat("inflow missing: ")
@@ -103,14 +100,17 @@ check_reachability <- function(N, path_M) {
 # Function calculates the reaction rates given a set of pathways and their
 # associated rates.
 
-reconstruct_rates <- function(path_M, path_rates) {
+pa_reconstruct_rates <- function(path_M, path_rates) {
     return(as.vector(t(path_M) %*% path_rates))
 }
 
 
-# Helper function for do_subpath_decompostion
+# Helper function for pa_subpath_decompostion
+# The function checks in which rows of the matrix <path_M> the same elements
+# are non-zero than in the vector <v> and returns a boolean vector of length
+# nrow(path_M)
 
-is_row_contained <- function(v, path_M) {
+pa_is_row_contained <- function(v, path_M) {
     if(!is.matrix(path_M))
         return(c(F))
 
@@ -121,17 +121,22 @@ is_row_contained <- function(v, path_M) {
 }
 
 
-do_subpath_decomposition <- function(N_orig, path_orig) {
+# Given a reaction network <N_orig> and a subset of active reactions <path_orig>
+# this function calculates all elementary flux modes in the subset of reactions.
+# This function follows the original efm-algorithm (TODO: source) and can only be
+# used for small networks. It is used as a helper for the function which is usable
+# for bigger networks.
+
+pa_subpath_decomposition <- function(N_orig, path_orig) {
     # calculate the reaction set for which decomposition is done
     sel_rea <- which(path_orig != 0)
+
     # only species that are created and consumed by above reaction set are 
     # taken as branching species
 
     N <- matrix(N_orig[,sel_rea], nrow(N_orig), length(sel_rea))
-    #branch_sp <- which(apply(N, 1, max) > 0 & apply(N, 1, min) < 0)
-    branch_sp <- which(N %*% path_orig[sel_rea] == 0)
-        
-
+    branch_sp <- which(apply(N, 1, max) > 0 & apply(N, 1, min) < 0)
+ 
     if(length(branch_sp) == 0)
         return(path_orig)
 
@@ -157,13 +162,13 @@ do_subpath_decomposition <- function(N_orig, path_orig) {
         for(src in cr) {
             K_src <- abs((N %*% path_M[src,])[i])              # How does the pathway src change i
             for(dest in con) {
-                K_dst <- abs((N %*% path_M[dest,])[i])        # How does the pathway dest change i 
+                K_dst <- abs((N %*% path_M[dest,])[i])         # How does the pathway dest change i 
                 np <- K_dst*path_M[src,] +                          # new pathway
                       K_src*path_M[dest,]
                 np <- np/vec_gcd(np)
                 hull <- (path_M[dest,] != 0 | path_M[src,] != 0) 
 
-                if(!any(is_row_contained(hull, path_M_new)))
+                if(!any(pa_is_row_contained(hull, path_M_new)))
                     path_M_new <- rbind(path_M_new,matrix(np, 1, length(sel_rea)))                
             }
         }
@@ -205,7 +210,7 @@ check_pathway_present <- function(path_M, path_rates, path_M_el) {
 
 
 
-#
+# 
 #
 #
 
@@ -267,11 +272,11 @@ pathway_analysis_prototype <- function(net, rates, fexp=0.001, f2=1e-40) {
         cat("branching at species ", net[[1]]$name[i], "\n")
 
         # calculate maximal relative deviation of reaction rates
-        mm <- max(abs((reconstruct_rates(path_M, path_rates)-rates)/rates), na.rm=T)
+        mm <- max(abs((pa_reconstruct_rates(path_M, path_rates)-rates)/rates), na.rm=T)
         # which element has this maximal relative deviation?
-        w_mm <- which(mm == abs((reconstruct_rates(path_M, path_rates)-rates)/rates))[1]
+        w_mm <- which(mm == abs((pa_reconstruct_rates(path_M, path_rates)-rates)/rates))[1]
         # print check:  maximal relative deviation;  reaction id; absolute 
-        cat("MCHECK: ",  mm, "  - ", (reconstruct_rates(path_M, path_rates)-rates)[w_mm] , "  - ",  w_mm ,  "\n")
+        cat("MCHECK: ",  mm, "  - ", (pa_reconstruct_rates(path_M, path_rates)-rates)[w_mm] , "  - ",  w_mm ,  "\n")
 
         # net creator
         x <- N %*% t(path_M)
@@ -281,8 +286,6 @@ pathway_analysis_prototype <- function(net, rates, fexp=0.001, f2=1e-40) {
         cr_f <- (cr_m*path_rates[cr])/rate_gen
        
         # net consumer
-        #check_reachability(N, path_M)
-
         con <- which(x[i,] < 0)
         con_m <- -x[i,con]
         con_f <- con_m*path_rates[con]/rate_gen
@@ -340,7 +343,6 @@ pathway_analysis_prototype <- function(net, rates, fexp=0.001, f2=1e-40) {
                 K_src <- cr_m[src]             # How does the pathway src change i
                 cat(".")
                 for(dest in 1:length(con)) {
-                    #cat("MCHECK: ",  max(abs(reconstruct_rates(path_M_new, path_rates_new)-rates)/rates, na.rm=T), "\n")
 
                     K_dst <- con_m[dest]       # How does the pathway dest change i 
                     nr <- z[src,dest]*rate_gen/(K_dst*K_src)                    # new rate
@@ -362,7 +364,7 @@ pathway_analysis_prototype <- function(net, rates, fexp=0.001, f2=1e-40) {
                         }
 
                         # First calculate pathway decomposition
-                        path_M_dec <- do_subpath_decomposition(N, np)
+                        path_M_dec <- ps_subpath_decomposition(N, np)
                         #path_M_dec <- matrix(np, ncol=length(np))
  
                         # 
@@ -519,7 +521,7 @@ pathway_analysis_alternative <- function(net, rates, f2=1e-40, comp=50) {
                         }
 
                         # First calculate pathway decomposition
-                        path_M_dec <- do_subpath_decomposition(N, np)
+                        path_M_dec <- pa_subpath_decomposition(N, np)
                         
                         # returns boolean array of EMs already present (p_sort$present)
                         p_sort <- check_pathway_present(path_M_new, path_rates_new, path_M_dec)
@@ -665,7 +667,7 @@ pathway_analysis_alternative2 <- function(net, rates, f2=1e-40, comp=50) {
                         }
 
                         # First calculate pathway decomposition
-                        path_M_dec <- do_subpath_decomposition(N, np)
+                        path_M_dec <- pa_subpath_decomposition(N, np)
                         
                         # returns boolean array of EMs already present (p_sort$present)
                         p_sort <- check_pathway_present(path_M_new, path_rates_new, path_M_dec)
@@ -812,7 +814,7 @@ pathway_analysis_alternative3 <- function(net, rates, f2=1e-40, C1=6, C2=10) {
                         }
 
                         # First calculate pathway decomposition
-                        path_M_dec <- do_subpath_decomposition(N, np)
+                        path_M_dec <- pa_subpath_decomposition(N, np)
                         
                         # returns boolean array of EMs already present (p_sort$present)
                         p_sort <- check_pathway_present(path_M_new, path_rates_new, path_M_dec)
@@ -892,11 +894,11 @@ pathway_analysis_prototype2 <- function(net, rates, fexp=0.001, f2=1e-10, C1=6, 
         cat("branching at species ", net[[1]]$name[i], "\n")
 
         # calculate maximal relative deviation of reaction rates
-        mm <- max(abs((reconstruct_rates(path_M, path_rates)-rates)/rates), na.rm=T)
+        mm <- max(abs((pa_reconstruct_rates(path_M, path_rates)-rates)/rates), na.rm=T)
         # which element has this maximal relative deviation?
-        w_mm <- which(mm == abs((reconstruct_rates(path_M, path_rates)-rates)/rates))[1]
+        w_mm <- which(mm == abs((pa_reconstruct_rates(path_M, path_rates)-rates)/rates))[1]
         # print check:  maximal relative deviation;  reaction id; absolute 
-        cat("MCHECK: ",  mm, "  - ", (reconstruct_rates(path_M, path_rates)-rates)[w_mm] , "  - ",  w_mm ,  "\n")
+        cat("MCHECK: ",  mm, "  - ", (pa_reconstruct_rates(path_M, path_rates)-rates)[w_mm] , "  - ",  w_mm ,  "\n")
 
         # net creator
         x <- N %*% t(path_M)
@@ -906,8 +908,6 @@ pathway_analysis_prototype2 <- function(net, rates, fexp=0.001, f2=1e-10, C1=6, 
         cr_f <- (cr_m*path_rates[cr])/rate_gen
        
         # net consumer
-        #check_reachability(N, path_M)
-
         con <- which(x[i,] < 0)
         con_m <- -x[i,con]
         con_f <- con_m*path_rates[con]/rate_gen
@@ -964,8 +964,6 @@ pathway_analysis_prototype2 <- function(net, rates, fexp=0.001, f2=1e-10, C1=6, 
                 K_src <- cr_m[src]             # How does the pathway src change i
                 cat(".")
                 for(dest in 1:length(con)) {
-                    #cat("MCHECK: ",  max(abs(reconstruct_rates(path_M_new, path_rates_new)-rates)/rates, na.rm=T), "\n")
-
                     K_dst <- con_m[dest]       # How does the pathway dest change i 
                     nr <- z[src,dest]*rate_gen/(K_dst*K_src)                    # new rate
                     np <- K_dst*path_M[cr[src],] +                          # new pathway
@@ -986,7 +984,7 @@ pathway_analysis_prototype2 <- function(net, rates, fexp=0.001, f2=1e-10, C1=6, 
                         }
 
                         # First calculate pathway decomposition
-                        path_M_dec <- do_subpath_decomposition(N, np)
+                        path_M_dec <- pa_subpath_decomposition(N, np)
                         #path_M_dec <- matrix(np, ncol=length(np))
  
                         # 
@@ -1045,20 +1043,28 @@ pathway_analysis_prototype2 <- function(net, rates, fexp=0.001, f2=1e-10, C1=6, 
 } 
 
 
+# The function checks in a matrix of elementary modes if they really are elementary
+# (by decomposing it with pa_subpath_decomposition) and removes multiple occurences.
 
-
-
-em_rm_multiple_check_elementary <- function(net, ems) {
+pa_rm_multiple_check_elementary <- function(net, ems) {
+    # cut these reactions of elementary modes that are not in reaction network
     ems <- ems[,1:nrow(net[[2]])]
+
+    # new matrix / ems that are checked are put here
     em_new <- ems[c(),]
-    N <- jrnf_calculate_stoich_mat(net)
+    N <- jrnf_calculate_stoich_mat(net)     # stoichiometric matrix
 
     for(i in 1:nrow(ems)) {
         cat(".")
-        path_M_dec <- do_subpath_decomposition(N, ems[i,])
-        if(nrow(path_M_dec) != 1)
-            cat("WARNING: Mode number ", i, " was not elementary!\n")
+        path_M_dec <- pa_subpath_decomposition(N, ems[i,])
 
+        # Show warning if decomposition into elementary modes gives more than em back.
+        # This doesn't mean that ems[i,] was not an elementary mode. Could be the same
+        # set of reactions is involved in different elementary modes (different coefficients)!
+        if(nrow(path_M_dec) != 1)
+            cat("WARNING: Mode number ", i, " may not have been elementary!\n")
+
+        # If not already in em_new add the pathway
         for(j in 1:nrow(path_M_dec)) {
             cpp <- check_pathway_present(em_new, rep(1, nrow(em_new)), matrix(path_M_dec[j,],1))
             if(!any(cpp$present))  # adding
@@ -1069,74 +1075,6 @@ em_rm_multiple_check_elementary <- function(net, ems) {
 
     return(em_new)
 }
-
-
-
-em_sort_by_rate <- function(ems, v) {
-    ems <- ems[,1:length(v)]
-
-    al <- function(x) {
-        y <- v/x
-        if(length(which(y>0)) > 0) 
-            return(min(y[x>0]))
-        else
-            return(0)
-    }
-
-    a <- apply(ems, 1, al)
-
-    return(ems[order(a,decreasing=T),])
-}
-
-
-
-em_develop_rates_max <- function(ems, v) {
-    ems <- matrix(ems[,1:length(v)], ncol=length(v))
-    em_new <- ems[c(),]
-    coeff <- c()
-    non_zero <- v != 0
-
-    finished <- !is.matrix(ems) | length(v) == 0
-
-    al <- function(x) {
-        y <- v/x
-        if(length(which(y>0)) > 0) 
-            return(min(y[x>0]))
-        else
-            return(0)
-    }
-
-    while(!finished) {
-        a <- apply(ems, 1, al)
-        m_id <- order(a, decreasing=T)[1]
-
-        #y <- v/ems[m_id,]
-        m_rate <- a[m_id]
-        #lim_rea <- which(y[ems[m_id,]>0] == m_rate)[1]
-
-        cat("coefficient = ", m_rate, "\n")
-
-        if(m_rate != 0) {
-            coeff <- c(coeff, m_rate)
-            v <- v - ems[m_id,]*m_rate
-            v[v<0] <- 0
-            #v[lim_rea] <- 0
-
-            em_new <- rbind(em_new, ems[m_id,])   # add elementary mode to new elementary modes
-            ems <- ems[-m_id,]                    # remove it from old list
-            #ems <- ems[ems[,lim_rea] == 0,]       # also remove all elementary modes that have the limiting (now zero) reaction
-        }
-
-       finished <- !is.matrix(ems) | length(v) == 0 | m_rate == 0
-    }
-
-    # Return new list / matrix of elementary modes  +  coefficients  + 
-    return(list(em_new, coeff, v))  
-}
-
-
-
-
 
 
 # This function orders the elementary modes and generates a data frame containing information
