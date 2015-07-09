9a5bdfdafeca9b5db36fbc9fea5c5f4cd120bd2a
Jakob Fischer <jakob@automorph.info>
2015-07-09 17:03:09 +0900

    added header and documentation to tools.R

--- tools.R
+++ tools.R
@@ -1,3 +1,12 @@
+# author: jakob fischer (jakob@automorph.info)
+# date: 9. July 2015
+# description: 
+# little helper functions that implement general functions that are not sufficiently
+# available through the language
+
+
+# function "acc" calculates the cummulative sum of numeric vectors
+
 acc <- function(x) {
     a <- 0
     res <- rep(0, length(x))
