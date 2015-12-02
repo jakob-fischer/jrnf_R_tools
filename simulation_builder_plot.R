2da7bb29b5e515407c6f8aff78748ef004b37e04
Jakob Fischer <jakob@automorph.info>
2015-12-02 23:57:21 +0900

    added files for simulation builder (ecosystem simulation generator)

---
+++ simulation_builder_plot.R
@@ -0,0 +1,9 @@
+# author: jakob fischer (jakob@automorph.info)
+# date: 2nd December 2015
+# description: 
+# Script for generating files and directory structure of artificial chemistry
+# simulations with thermodynamic constraints. (simulation builder = "sb_")
+# This file contains the functilonality to plot (and partially evaluate) the results.
+# The core functionality is in "simulation_builder.R".
+
+sourced_simulation_builder_plot <- T
