85ba7a467d5b1da1ebc4d257ea9a65a083b2f177
Jakob Fischer <jakob@automorph.info>
2016-09-26 07:29:25 +0200

    collect data for evolution, make pw-dec optional

--- simulation_builder_evol.R
+++ simulation_builder_evol.R
@@ -15,8 +15,8 @@ sourced_simulation_builder_evol <- T
 # <con_a>  - initial concentration (before shuffling) of anorganic species
 # <con_o>  - initial concentration (before shuffling) of species related to organisms
 # <con_hv> - concentration of driving / hv species
-# <Tmax>   - 
-# <wint>   -
+# <Tmax>   - time to which is simulated
+# <wint>   - number of intervalls that the results is written to file
 
 sb_solve_ae_org <- function(net, con_a, con_o, con_hv, Tmax, wint) {
     
@@ -87,7 +87,7 @@ sb_solve_ae_org <- function(net, con_a, con_o, con_hv, Tmax, wint) {
 #
 #
 
-sb_ae_evo_score <- function(net, result) {
+sb_ae_evo_score <- function(net, result, eval_worst=(function (r) which.min(r$flux_org)), do_pw_ana=T) {
     subsum <- function(a, b) {
         max_b <- max(b)
         x <- rep(0, max_b)
@@ -96,10 +96,21 @@ sb_ae_evo_score <- function(net, result) {
         return(x)
     }
 
+    # Calculates the flux that one part (specific organism, anorganic) of the 
+    # network has with the other parts. For this the network part is separated
+    # and all effective in- and out-fluxes are transformed to elementary 
+    # components and all of them are summed up.
+    #
+    #
+    # 
     subflux <- function(N_rev, rates_rev, re_assoc, i_i) {
         comp <- net$composition
         comp[net[[1]]$name == "hv",1] <- 1
         x <- c()
+        cat("re_assoc=", re_assoc, "  - len(..)=", length(re_assoc), "\n")
+        cat("i_i=", i_i, "\n")
+        cat("dim(N_rev)=", dim(N_rev), "\n")
+ 
         for(i in i_i) {
             dx <- abs(matrix(N_rev[,re_assoc == i], nrow=nrow(N_rev)) %*% rates_rev[re_assoc == i])
             x <- c(x, sum(t(comp) %*% dx))
@@ -110,24 +121,40 @@ sb_ae_evo_score <- function(net, result) {
     # conclude pathway analysis
     hv_id <- which(net[[1]]$name == "hv")
 
-    x <- pa_extend_net(net, result$flow$flow_effective, invwhich(hv_id), T)
+    cat("net - size: ", nrow(net[[1]]), " - ", nrow(net[[2]]), "\n")
+
+    x <- pa_extend_net(net, result$flow$flow_effective, invwhich(hv_id, nrow(net[[1]])), T)
     net_ex <- x[[1]]
     rates_ex <- x[[2]]
+
+
+    cat("net_ex - size: ", nrow(net_ex[[1]]), " - ", nrow(net_ex[[2]]), "\n")
 
     net_rev <- jrnf_reverse_reactions(net_ex, rates_ex)
     N_rev <- jrnf_calculate_stoich_mat(net_rev)
     rates_rev <- abs(rates_ex)
-    res_ana <- pa_analysis(net_rev, rates_rev, 0.001, 0.7)
-    a <- pa_calc_coefficients(res_ana[[1]], rates_rev, T)
+    # TODO
+    # res_ana <- pa_analysis(net_rev, rates_rev, 0.001, 0.7)
+
+    if(do_pw_ana) {
+        res_ana <- pa_analysis(net_rev, rates_rev, 0.01, 0.7)
+        a <- pa_calc_coefficients(res_ana[[1]], rates_rev, T)
+
+    } else {
+        res_ana <- list(1) # pa_analysis(net_rev, rates_rev, 0.01, 0.7)
+        a <- list(coef=0)  #pa_calc_coefficients(res_ana[[1]], rates_rev, T)
+    }
 
     pathways <- list()
     pathways$net_ex <- net_ex
     pathways$rates_ex <- rates_ex
-    pathways$em <- a[[1]]
-    pathways$em_rates <- a[[2]]
+    pathways$em <- res_ana[[1]]
+    pathways$em_coef <- a$coef
 
-    for(l in 1:nrow(pathways$em))
-        pathways$em[l,] <- pathways$em[l,]*(sign(rates_ex))
+    if(do_pw_ana) {
+        for(l in 1:nrow(pathways$em))
+            pathways$em[l,] <- pathways$em[l,]*(sign(rates_ex))
+    }
 
     # for each organism calculate exchange fluxes
     # and calculate explained fraction of steady state 
@@ -135,21 +162,24 @@ sb_ae_evo_score <- function(net, result) {
     rates <- list()     
 
     # sum of rates (total, organisms, anorganic)
+    # The pseudoreaction adding hv is accounted as anorganic. 
     re_assoc_ex <- c(net$assoc$re, 0)
-    rates$rate_tot <- sum(rates_rev)
-    rates$rate_org <- subsum(rates_rev, re_assoc_ex)
-    rates$rate_anorg <- rates$rates_tot - sum(rates$rates_org)
+    rates$rates_tot <- sum(rates_rev)                            # total accumulated rate
+    rates$rates_org <- subsum(rates_rev, re_assoc_ex)            # vector containing accumulated rate of all
+                                                                # reactions associated with different organisms
+    rates$rate_anorg <- rates$rates_tot - sum(rates$rates_org)  # accumulated rate of anorganic part
  
     # sum of fluxes (total, organisms, anorganic)
-    
-    rates$flux_org <- subflux(N_rev, rates_rev, re_assoc_ex, 0)
-    rates$flux_anorg <- subflux(N_rev, rates_rev, re_assoc_ex, 1:max(re_assoc_ex))
-    rates$flux_tot <- rates$flux$org + flux_anorg
+    rates$flux_anorg <- subflux(N_rev, rates_rev, re_assoc_ex, 0)
+    rates$flux_org <- subflux(N_rev, rates_rev, re_assoc_ex, 1:max(re_assoc_ex))
+    rates$flux_tot <- sum(rates$flux_org) + rates$flux_anorg
 
     # fraction of rates (organisms) - in terms of total rates
     rates$rates_org_f <- rates$rates_org / rates$rates_tot
-    rates$fluxes_org_f <- rates$fluxes_org / rates$fluxes_tot
-    rates$org_cycling_f <- rates$rates_org / rates$fluxes_org
+    rates$flux_org_f <- rates$flux_org / rates$flux_tot   # fraction a organism has on total flux
+    rates$org_cycling_f <- rates$rates_org / rates$flux_org   # cycling ratio defined by ratio of rates by flux
+                     # Because thermodynamically rates has to be driven by fluxes a high fraction implies there
+                     # has to be a lot of cycling.
 
 
     # for each organism calculate mass (fraction)
@@ -160,12 +190,21 @@ sb_ae_evo_score <- function(net, result) {
     weight$total <- sum(weight$con)
     weight$org <- subsum(weight$con, net$assoc$sp)
     weight$anorg <- weight$total - sum(weight$org) 
+    weight$org_f = weight$org/weight$total
 
     # 
-    result$eval_worst_id <- which.min(w_org)
-    results$eval_pathways <- pathways
-    results$eval_weight <- weight
-    results$eval_rates <- rates
+    result$eval_pathways <- pathways
+    result$eval_weight <- weight
+    result$eval_rates <- rates
+    result$eval_worst_id <- eval_worst(result)
+    
+    cat(" weight (fraction) of org.:", weight$org_f, "\n")
+    cat(" flux (fraction) of org.:", rates$flux_org_f, "\n")
+    cat(" rates(fraction) of org.:", rates$rates_org_f, "\n")
+    cat(" cycling fraction of org.:", rates$org_cycling_f, "\n")
+    cat(" worst is organism with id ", result$eval_worst_id, "\n")
+    cat("-----------------------------------------------------------\n")
+
 
     return(result)
 }
@@ -180,14 +219,12 @@ sb_ae_evo_score <- function(net, result) {
 # <net_ac> - anorganic network 
 # <no_o>   - number of organisms
 # <no_gen> - number of generations
-# <score>  - score function that gets a extended network (containing added 
-#            organisms and a results object - from sb_solve_ae_org) and 
-#            calculates all possible and necessary scores. This can be
-#            customized, but important is the returned object to have a
-#            field <$eval_worst_id> that contains the id of the organism 
-#            that scores worst and should be replaced in the next generation.
+# <eval_worst>  
+#          - score function that i used by sb_ae_evo_score to evaluate which
+#            of the organisms can be dropped in the next simulation step. 
+#            (see above)
 
-sb_ae_org_evolve <- function(net_ac, no_o, no_gen, score) {
+sb_ae_org_evolve <- function(net_ac, no_o, no_gen, eval_worst, do_pw_ana=T) {
     # Submethod calling <sb_solve_ae_org> to calculate initial conditions and
     # simulate the differential equation. The parameters are hardcoded now 
     # but that might be changed later. 
@@ -207,6 +244,18 @@ sb_ae_org_evolve <- function(net_ac, no_o, no_gen, score) {
     # List of extended networks and corresponding simulations
     net_list <- list()
     res_list <- list()
+    dyn <- list()
+    dyn$worst_id <- c()
+    dyn$weight_f_anorg <- c()
+    dyn$flux_anorg <- c()
+    dyn$flux_f_max <- c()
+    dyn$rates_f_max <- c()
+    dyn$weight_f_max <- c()
+    dyn$cycling_f_max <-c()
+    dyn$flux_f_mean <- c()
+    dyn$rates_f_mean <- c()
+    dyn$weight_f_mean <- c()
+    dyn$cycling_f_mean <-c()
 
     # Do simulation loom <no_gen> times
     for(i in 1:no_gen) {
@@ -215,16 +264,41 @@ sb_ae_org_evolve <- function(net_ac, no_o, no_gen, score) {
         cat("-----------------------------------------------------\n")
  
         ext <- jrnf_ae_add_organism(ext)   # add one additional organism
+
+        if(nrow(ext[[2]]) != length(ext$assoc$re)) {
+            cat("BAD LENGTH MISMATCH in ASSOC RE (AA):\n")
+            return(ext)
+        }
         res <- solve(ext)                  # solve
-        res_eval <- score(ext, res)        # evaluate result
+        res_eval <- sb_ae_evo_score(ext, res, eval_worst, do_pw_ana)        # evaluate result
+        cat("EVALUATED!\n")
+
+        dyn$worst_id <- c(dyn$worst_id, res_eval$eval_worst_id)
+        dyn$weight_f_anorg <- c(dyn$weight_f_anorg, res_eval$eval_weight$anorg/res_eval$eval_weight$total)
+        dyn$flux_anorg <- c(dyn$flux_anorg, res_eval$eval_rates$flux_anorg)
+        dyn$flux_f_max <- c(dyn$flux_f_max, max(res_eval$eval_rates$flux_org_f))
+        dyn$rates_f_max <- c(dyn$rates_f_max, max(res_eval$eval_rates$rates_org_f))
+        dyn$weight_f_max <- c(dyn$weight_f_max, max(res_eval$eval_weight$org_f))
+        dyn$cycling_f_max <-c(dyn$cycling_f_max, max(res_eval$eval_rates$org_cycling_f))
+        dyn$flux_f_mean <- c(dyn$flux_f_mean, mean(res_eval$eval_rates$flux_org_f))
+        dyn$rates_f_mean <- c(dyn$rates_f_mean, mean(res_eval$eval_rates$rates_org_f))
+        dyn$weight_f_mean <- c(dyn$weight_f_mean, mean(res_eval$eval_weight$org_f))
+        dyn$cycling_f_mean <-c(dyn$cycling_f_mean, mean(res_eval$eval_rates$org_cycling_f))
 
         net_list[[length(net_list)+1]] <- ext         # list collecting data
         res_list[[length(res_list)+1]] <- res_eval    # list collecting data
 
+        #return(list(ext, res_eval))
+
         # kill off worst organism
         ext <- jrnf_ae_remove_organism(ext, res_eval$eval_worst_id)
+
+        if(nrow(ext[[2]]) != length(ext$assoc$re)) {
+            cat("BAD LENGTH MISMATCH in ASSOC RE (BB):\n")
+            return(ext)
+        }
     } 
 
-    return(list(net_list=net_list, res_list=res_list))
+    return(list(net_list=net_list, res_list=res_list, dyn_data=dyn))
 }
 
