e11c40f565f69ff55f8323e0770b7882f764882d
Jakob Fischer <jakob@automorph.info>
2016-12-06 13:57:40 +0100

    documentation

--- simulation_builder_flow.R
+++ simulation_builder_flow.R
@@ -123,26 +123,32 @@ sb_collect_results <- function(b1, b2) {
                      ep_max=numeric(), sp_df=I(list()), re_df=I(list()), 
                      last_time=numeric(), last_msd=numeric())
 
-    # 
+    # Collect folders' names - Each corresponds to one simulation of the same
+    # network with a different set of energies.
     Edir_v <- list.dirs(recursive=FALSE)
 
 
     for(bp in Edir_v) {
         setwd(bp)
+        # Load networks (because they contain energy set).
         net <- jrnf_read("net_energies.jrnf")
         bp <- as.numeric(strsplit(bp,"/")[[1]][2])
 
+        # Collect subfolders' names - they contain the boundary species' concentration.
         vdir_v <- list.dirs(recursive=FALSE)
         for(vp in vdir_v) {
+            # Extract concentrations.
             l <- strsplit(vp, "v")[[1]][2]
             v1 <- as.numeric(strsplit(l, "_")[[1]][1])
             v2 <- as.numeric(strsplit(l, "_")[[1]][2])
             setwd(vp)
 
+            # Every file corresponds to one simulation run.
             edir_v <- list.files(recursive=FALSE)
             for(ep in edir_v) {
                 i <- as.numeric(strsplit(ep,"\\.")[[1]][1])
                 
+                # Read file
                 cat("doing b1=", b1, "b2=", b2, "v1=", v1, "v2=", v2, "Edraw=", as.numeric(bp), "Rdraw=", i, "\n")
                 run <- read.csv(ep)
 
@@ -150,23 +156,31 @@ sb_collect_results <- function(b1, b2) {
                 last_msd_  <- run[nrow(run),2]
                 last_con <- data.frame(con=as.numeric(run[nrow(run), 3:ncol(run)]))
 
-               
+                # Calculate flow (steady state rates)
                 last_flow <- jrnf_calculate_flow(net, last_con$con)
 
+                # Use concentration change to calculate exchange flow through 
+                # both boundary species.
                 cc <- jrnf_calculate_concentration_change(net, last_flow$flow_effective)
                 flowb1 <- cc[b1]
                 flowb2 <- cc[b2]
+                # Estimate absolute error from "concentration change" of non 
+                # exchange species and relative error by comparing this to
+                # the flow through the system.
                 err_cc <- max(abs(cc[c(-b1,-b2)]))
                 flow <- 0.5*abs(flowb1-flowb2)
                 err_cc_rel <- err_cc/flow
 
+                # Add run to data frame
                 df <- rbind(df,
-                              data.frame(Edraw=as.numeric(bp), Rdraw=as.numeric(i), b1=as.numeric(b1), 
-                              b2=as.numeric(b2), v1=as.numeric(v1), v2=as.numeric(v2),
-                              flow1=as.numeric(flowb1), flow2=as.numeric(flowb2), flow=flow, err_cc=err_cc,
-                              err_cc_rel=err_cc_rel,
+                              data.frame(Edraw=as.numeric(bp), Rdraw=as.numeric(i),
+                              b1=as.numeric(b1), b2=as.numeric(b2), 
+                              v1=as.numeric(v1), v2=as.numeric(v2),
+                              flow1=as.numeric(flowb1), flow2=as.numeric(flowb2), 
+                              flow=flow, err_cc=err_cc, err_cc_rel=err_cc_rel,
                               ep_tot=as.numeric(sum(last_flow$entropy_prod)), 
-                              sp_df=I(list(last_con)), re_df=I(list(last_flow)), last_time=last_time_, last_msd=last_msd_))
+                              sp_df=I(list(last_con)), re_df=I(list(last_flow)), 
+                              last_time=last_time_, last_msd=last_msd_))
             }
  
             setwd("..")
@@ -203,15 +217,19 @@ sb_em_analysis <- function(res, net, c_max=4) {
     re <- res$re_df
     em_ex <- list()
 
-    # 
+    # Cycles (matrix and sum)
     C_matrix <- matrix(0, ncol=c_max, nrow=nrow(res))  
     C_sum <- rep(0, nrow(res))
+    # Shortest pathways
     sh_p <- rep(0, nrow(res))
-    sh_p_d <- rep(0, nrow(res))
+    sh_p_d <- rep(0, nrow(res)) # directed
+    # Entropy production (linear / nonlinear reactions)
     ep_l <- rep(0,nrow(res))
     ep_nl <- rep(0,nrow(res))
+    # Error
     err_rel_max <- rep(0,nrow(res))
     err <- rep(0,nrow(res))
+    # Number of pathways / elementary modes
     em_no <- rep(0, nrow(res))
 
 
@@ -229,28 +247,28 @@ sb_em_analysis <- function(res, net, c_max=4) {
         cat("============================================================\n")
         cat(" i=", i, "  b1=", res$b1[i], "  b2=", res$b2[i], "  v1=", res$v1[i], "  v2=", res$v2[i], "\n")  
 
-        # calculating directed network
+        # Calculating directed network
         gc()
         g <- jrnf_to_directed_network_d(net, res$re_df[[i]]$flow_effective)
         cat(".")         
 
-        # calculating elementary modes
+        # Calculate reverse network with all rates positive
         rev <- res$re_df[[i]]$flow_effective
         rates_rev <- abs(res$re_df[[i]]$flow_effective) 
         net_rev <- jrnf_reverse_reactions(net, rev)
       
         cdif_r <- jrnf_calculate_concentration_change(net_rev, rates_rev)
 
-        # add reactions to balance growth / decrease
+        # Add reactions to balance growth / decrease
         for(k in c(res$b1[i], res$b2[i])) 
-            # if species' concentration increases one pseudoreaction has to be included to remove it ("X -> ")
+            # If species' concentration increases one pseudoreaction has to be included to remove it ("X -> ")
             if(cdif_r[k] > 0) {   
 	        net_rev[[2]] <- rbind(net_rev[[2]], data.frame(reversible=factor(c(FALSE)), 
                                       c=as.numeric(c(1)), k=as.numeric(c(1)),k_b=as.numeric(c(0)), 
                                       activation=as.numeric(c(0)),educts=I(list(k)), educts_mul=I(list(1)),
                                       products=I(list(c())), products_mul=I(list(c()))))
                 rates_rev <- c(rates_rev, cdif_r[k])
-            # if species' concentration decreases one pseudoreaction is included to add it ("-> X ")
+            # If species' concentration decreases one pseudoreaction is included to add it ("-> X ")
             } else { 
 	         net_rev[[2]] <- rbind(net_rev[[2]], data.frame(reversible=factor(c(FALSE)), 
                                        c=as.numeric(c(1)), k=as.numeric(c(1)),k_b=as.numeric(c(0)), 
@@ -259,16 +277,13 @@ sb_em_analysis <- function(res, net, c_max=4) {
                 rates_rev <- c(rates_rev, -cdif_r[k])
             }
 
-        #x_em <- pa_analysis(net_rev, rates_rev, param$fext, param$pmin, T, F)[[1]]
-        x <- pa_analysis(net_rev, rates_rev, 0, 0)
+        # Calculate pathways and coefficients
+        x_em <- pa_analysis(net_rev, rates_rev, param$fext, param$pmin, T, F)[[1]
+        x_rates <- pa_calculate_coef(x_em, rates_rev)$coef
 
-        # rename results and sort decreasing with fraction of v explained...
-        x_em <- x[[1]]
-        x_rates <- x[[2]]
+        # Sort decreasing with fraction of v explained...
         x_sum <- apply(x_em, 1, sum)
         o <- order(x_rates*x_sum, decreasing=T)
-
-
         x_em <- matrix(x_em[o,], ncol=ncol(x_em))
         x_rates <- x_rates[o] 
         x_sum <- x_sum[o]
@@ -286,7 +301,7 @@ sb_em_analysis <- function(res, net, c_max=4) {
 
         if(nrow(x_em) > 0)
         for(l in 1:nrow(x_em)) {
-            # 
+            # Calculate explained rate and explained dissipation
             exp_r[l] <- sum(x_em[l,]*x_rates[l])/sum(rates_rev)
             exp_d[l] <- x_em[l,length(rates_rev)]*x_rates[l] / rates_rev[length(rates_rev)]
 
@@ -309,6 +324,7 @@ sb_em_analysis <- function(res, net, c_max=4) {
         em_ex[[i]] <- data.frame(id=em_id, rate=x_rates, exp_r=exp_r, exp_r_cum=cumsum(exp_r), 
                                  exp_d=exp_d, exp_d_acc=cumsum(exp_d))
 
+
         # calculation of cycles 
         for(j in 1:c_max) 
             C_matrix[i,j] <- get_n_cycles_directed(g,j)[[1]]
@@ -325,7 +341,6 @@ sb_em_analysis <- function(res, net, c_max=4) {
             sh_p_d[i] <- matrix(shortest.paths(g, mode="out"), ncol=N_sp, nrow=N_sp)[res$b2[i], res$b1[i]]
 
         # Entropy production of linear part and of nonlinear part
-
         ep_l[i] <- sum(res$re_df[[i]]$entropy_prod[is_linear])
         ep_nl[i] <- sum(res$re_df[[i]]$entropy_prod[!is_linear])
 
@@ -346,23 +361,23 @@ sb_em_analysis <- function(res, net, c_max=4) {
     res$re_df <- re
     res$sp_df <- sp
     res$em_ex <- em_ex
-    #res <- cbind(res, em_ex)
 
+    # Additional score to determine error
     res$reliability <- as.numeric(abs(abs(res$flow*log(res$v1/res$v2)) - res$ep_tot)/res$ep_tot > 0.1) + 1  
 
     res$shortest_path <- sh_p
     res$shortest_path_d <- sh_p_d
 
-    res$C1 <- C_matrix[,1]
-    res$C2 <- C_matrix[,2]
-    res$C3 <- C_matrix[,3]
-    res$C4 <- C_matrix[,4]
-    res$C_sum <- C_sum
+    for(k in 1:c_max) 
+        res[paste("C", as.character(k), sep="")] <- C_matrix[,k]
+
+    res$C_sum <- apply(C_matrix, 1, sum)
 
     res$em_no <- em_no
     res$em_err_max <- err_rel_max
     res$em_err_sum <- err
 
+    # Save and return data
     results_em <- res
     em_m <- em_matrix
     save(results_em, em_m, file="results_em.Rdata")
